ab5e6fc87067fb6c0039cfd4f610ea6172ac242c
manyajain27 <jainmanya2701@gmail.com>
2024-11-02 01:50:35 +0530

    commit

--- backend/settings.py
+++ backend/settings.py
@@ -56,7 +56,7 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=2),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=10),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=60),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
